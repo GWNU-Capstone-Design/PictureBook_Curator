4be2a9fc4336609ce7ac3dc224f807a3c831c84b
jyjh815 <132442313+jyjh815@users.noreply.github.com>
2024-04-16 15:56:49 +0900

    Update login_scripts.js
    비밀번호찾기 수정

--- login_scripts.js
+++ login_scripts.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     var showSignUp = document.getElementById('showSignUp');
-    var showLogin = document.getElementById('showLogin');
+    var findPW = document.getElementById("findPW");
+    var showLoginFromSignup = document.getElementById('showLoginFromSignup');
+    var showLoginFromFind = document.getElementById('showLoginFromFind');
     var loginForm = document.getElementById('loginForm');
     var signupForm = document.getElementById('signupForm');
+    var findForm = document.getElementById('findForm');
 
     showSignUp.addEventListener('click', function() {
         loginForm.style.display = 'none';
         signupForm.style.display = 'block';
     });
 
-    showLogin.addEventListener('click', function() {
+    showLoginFromSignup.addEventListener('click', function() {
         signupForm.style.display = 'none';
+        loginForm.style.display = 'block';
+    });
+
+    showLoginFromFind.addEventListener('click', function() {
+        findForm.style.display = 'none';
         loginForm.style.display = 'block';
     });
 
@@ -22,5 +30,10 @@ document.addEventListener('DOMContentLoaded', function() {
     signupForm.addEventListener('submit', function(event) {
         event.preventDefault();
         // 회원가입 로직
+    });
+
+    findPW.addEventListener('click', function() {
+        loginForm.style.display = 'none';
+        findForm.style.display = 'block';
     });
 });
