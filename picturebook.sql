223c1bf6f7ba0801c35a4bf0e561ddc90a25f3a2
HYOJIN138 <84240754+jeong-hyo-jin@users.noreply.github.com>
2024-04-23 20:59:10 +0900

    Update picturebook.sql

--- picturebook.sql
+++ picturebook.sql
@@ -1,136 +1,39 @@
 CREATE TABLE `User` (
-	`user_id`	INT(100)	NOT NULL,
-	`user_email`	VARCHAR(40)	NOT NULL,
-	`user_name`	VARCHAR(10)	NOT NULL,
-	`user_pw`	VARCHAR(20)	NOT NULL
+    `user_id` INT NOT NULL AUTO_INCREMENT,
+    `user_email` VARCHAR(40) NOT NULL,
+    `user_name` VARCHAR(10) NOT NULL,
+    `user_pw` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`user_id`)
 );
 
 CREATE TABLE `Book` (
-	`book_id`	INT(100)	NOT NULL,
-	`user_id`	INT(100)	NOT NULL,
-	`book_name`	VARCHAR(30)	NOT NULL
-);
-
-CREATE TABLE `Text` (
-	`text_id`	INT(100)	NOT NULL,
-	`image_id`	INT(100)	NOT NULL,
-	`book_id`	INT(100)	NOT NULL,
-	`user_id`	INT(100)	NOT NULL,
-	`text_value`	JSON	NOT NULL
+    `book_id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `book_name` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`book_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`)
 );
 
 CREATE TABLE `Image` (
-	`image_id`	INT(100)	NOT NULL,
-	`book_id`	INT(100)	NOT NULL,
-	`user_id`	INT(100)	NOT NULL,
-	`image_value`	VARCHAR(255)	NOT NULL'
+    `image_id` INT NOT NULL AUTO_INCREMENT,
+    `book_id` INT NOT NULL,
+    `image_value` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`image_id`),
+    FOREIGN KEY (`book_id`) REFERENCES `Book` (`book_id`)
+);
+
+CREATE TABLE `Text` (
+    `text_id` INT NOT NULL,
+    `image_id` INT NOT NULL,
+    `text_value` JSON,
+    PRIMARY KEY (`text_id`),
+    FOREIGN KEY (`image_id`) REFERENCES `Image` (`image_id`)
 );
 
 CREATE TABLE `Scenario` (
-	`scenario_id`	INT(100)	NOT NULL,
-	`text_id`	INT(100)	NOT NULL,
-	`image_id`	INT(100)	NOT NULL,
-	`book_id`	INT(100)	NOT NULL,
-	`user_id`	INT(100)	NOT NULL,
-	`scenario_value`	TEXT	NOT NULL
-);
-
-ALTER TABLE `User` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
-	`user_id`
-);
-
-ALTER TABLE `Book` ADD CONSTRAINT `PK_BOOK` PRIMARY KEY (
-	`book_id`,
-	`user_id`
-);
-
-ALTER TABLE `Text` ADD CONSTRAINT `PK_TEXT` PRIMARY KEY (
-	`text_id`,
-	`image_id`,
-	`book_id`,
-	`user_id`
-);
-
-ALTER TABLE `Image` ADD CONSTRAINT `PK_IMAGE` PRIMARY KEY (
-	`image_id`,
-	`book_id`,
-	`user_id`
-);
-
-ALTER TABLE `Scenario` ADD CONSTRAINT `PK_SCENARIO` PRIMARY KEY (
-	`scenario_id`,
-	`text_id`,
-	`image_id`,
-	`book_id`,
-	`user_id`
-);
-
-ALTER TABLE `Book` ADD CONSTRAINT `FK_User_TO_Book_1` FOREIGN KEY (
-	`user_id`
-)
-REFERENCES `User` (
-	`user_id`
-);
-
-ALTER TABLE `Text` ADD CONSTRAINT `FK_Image_TO_Text_1` FOREIGN KEY (
-	`image_id`
-)
-REFERENCES `Image` (
-	`image_id`
-);
-
-ALTER TABLE `Text` ADD CONSTRAINT `FK_Image_TO_Text_2` FOREIGN KEY (
-	`book_id`
-)
-REFERENCES `Image` (
-	`book_id`
-);
-
-ALTER TABLE `Text` ADD CONSTRAINT `FK_Image_TO_Text_3` FOREIGN KEY (
-	`user_id`
-)
-REFERENCES `Image` (
-	`user_id`
-);
-
-ALTER TABLE `Image` ADD CONSTRAINT `FK_Book_TO_Image_1` FOREIGN KEY (
-	`book_id`
-)
-REFERENCES `Book` (
-	`book_id`
-);
-
-ALTER TABLE `Image` ADD CONSTRAINT `FK_Book_TO_Image_2` FOREIGN KEY (
-	`user_id`
-)
-REFERENCES `Book` (
-	`user_id`
-);
-
-ALTER TABLE `Scenario` ADD CONSTRAINT `FK_Text_TO_Scenario_1` FOREIGN KEY (
-	`text_id`
-)
-REFERENCES `Text` (
-	`text_id`
-);
-
-ALTER TABLE `Scenario` ADD CONSTRAINT `FK_Text_TO_Scenario_2` FOREIGN KEY (
-	`image_id`
-)
-REFERENCES `Text` (
-	`image_id`
-);
-
-ALTER TABLE `Scenario` ADD CONSTRAINT `FK_Text_TO_Scenario_3` FOREIGN KEY (
-	`book_id`
-)
-REFERENCES `Text` (
-	`book_id`
-);
-
-ALTER TABLE `Scenario` ADD CONSTRAINT `FK_Text_TO_Scenario_4` FOREIGN KEY (
-	`user_id`
-)
-REFERENCES `Text` (
-	`user_id`
+    `scenario_id` INT NOT NULL AUTO_INCREMENT,
+    `text_id` INT NOT NULL,
+    `scenario_value` TEXT NOT NULL,
+    PRIMARY KEY (`scenario_id`),
+    FOREIGN KEY (`text_id`) REFERENCES `Text` (`text_id`)
 );
