1645a0114519a29e24dab20e923ce87f9965e291
wlgp <s412sjh@naver.com>
2024-05-08 11:39:44 +0900

    commit

--- PBC/script.js
+++ PBC/script.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const pinIcon = document.getElementById("pinIcon");
+    const restoreButton = document.getElementById("restoreButton");
     const header = document.querySelector("header");
     const footer = document.querySelector("footer");
 
     pinIcon.addEventListener("click", function() {
-        // 헤더와 푸터 숨기기
         header.style.display = "none";
         footer.style.display = "none";
+        restoreButton.style.display = "block"; // 복원 버튼을 보이게 함
+    });
+
+    restoreButton.addEventListener("click", function() {
+        header.style.display = "flex";
+        footer.style.display = "flex";
+        restoreButton.style.display = "none"; // 복원 버튼을 숨김
     });
 });
