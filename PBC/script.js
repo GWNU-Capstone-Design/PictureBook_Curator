e9d0c55bd7332b1b4d1413485c9fc67b3fa5866d
wlgp <s412sjh@naver.com>
2024-05-17 14:03:14 +0900

    밝기와 고정(돌아가기기능) 아이콘 수정해야함

--- PBC/script.js
+++ PBC/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var settingsIcon = document.getElementById("settingsIcon");
     var settingsModal = document.getElementById("settingsModal");
-    var closeBtn = document.querySelector('.close');
     var brightnessIcon = document.getElementById("brightnessIcon");
     var brightnessContainer = document.querySelector(".brightness-control-container");
     var pinIcon = document.getElementById("pinIcon");
@@ -12,15 +11,20 @@ document.addEventListener("DOMContentLoaded", function() {
     // Settings Modal Controls
     settingsIcon.onclick = function() {
         settingsModal.style.display = "block";
+        settingsModal.classList.remove("hidden");  // 'hidden' 클래스 제거
     };
 
-    closeBtn.onclick = function() {
+    // Correct close button for the settings modal
+    var settingsCloseBtn = settingsModal.querySelector('.close');
+    settingsCloseBtn.onclick = function() {
         settingsModal.style.display = "none";
+        settingsModal.classList.add("hidden");  // 'hidden' 클래스 추가
     };
 
     window.onclick = function(event) {
         if (event.target == settingsModal) {
             settingsModal.style.display = "none";
+            settingsModal.classList.add("hidden");  // 'hidden' 클래스 추가
         }
     };
 
