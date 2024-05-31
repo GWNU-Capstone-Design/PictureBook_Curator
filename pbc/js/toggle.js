af021791b4d6de5e8d9e435a8c53c3b9d8421ceb
jeong-hyo-jin <84240754+jeong-hyo-jin@users.noreply.github.com>
2024-05-31 12:23:31 +0900

    Add files via upload

---
+++ pbc/js/toggle.js
@@ -0,0 +1,10 @@
+function toggleText(event)
+{
+    var text = event.textContent || event.innerText;
+    if(text == '등록') {
+        event.innerHTML = '제목';
+    }
+    else {
+        event.innerHTML = '등록';
+    }
+}
