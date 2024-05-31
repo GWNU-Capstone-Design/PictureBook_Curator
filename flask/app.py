ef2b937165b17876ea8a156a9335915ef5eb128a
jyjh815 <132442313+jyjh815@users.noreply.github.com>
2024-05-31 15:19:10 +0900

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,20 +1,26 @@
 import os
 import pickle
 import numpy as np
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from google.cloud import texttospeech
+from flask import Flask, request, jsonify, render_template
+from openai import OpenAI
 from tensorflow.keras.models import load_model, Model
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Dropout, add
 
+# Flask 앱 초기화
 app = Flask(__name__)
+openai_client = OpenAI(api_key="")
+
+# Google 인증 정보 설정
+tts_client = texttospeech.TextToSpeechClient.from_service_account_json('./nth-transformer-420210-02d1185d9547.json')
 
 # 경로 설정
-BASE_DIR = "image"
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
+AUDIO_FOLDER = os.path.join('static', 'audio')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(AUDIO_FOLDER, exist_ok=True)
 
 WORKING_DIR = ""
 FEATURES_PATH = os.path.join(WORKING_DIR, 'features.pkl')
@@ -34,7 +40,7 @@ with open(TOKENIZER_PATH, 'rb') as f:
     tokenizer = pickle.load(f)
 
 vocab_size = len(tokenizer.word_index) + 1
-max_length = 10  # 최대 캡션 길이(모델 훈련할 때 설정한 값으로 꼭 해야함)
+max_length = 12  # 최대 캡션 길이(모델 훈련할 때 설정한 값으로 꼭 해야함)
 
 # 학습된 모델 로드
 model = load_model(MODEL_PATH)
@@ -66,39 +72,87 @@ def generate_caption(feature):
             break
     return input_text.replace('startseq', '').replace('endseq', '').strip()
 
+def text_to_speech(text, output_file):
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="ko-KR", 
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+    response = tts_client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+    with open(output_file, "wb") as out:
+        out.write(response.audio_content)
+    return output_file
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    if 'image' not in request.files:
-        return redirect(request.url)
+@app.route('/process_images', methods=['POST'])
+def process_images():
+    try:
+        if 'images' not in request.files:
+            return jsonify({'error': 'No images uploaded'}), 400
 
-    file = request.files['image']
-    if file.filename == '':
-        return redirect(request.url)
+        files = request.files.getlist('images')
+        if not files:
+            return jsonify({'error': 'No selected files'}), 400
 
-    if file:
-        img_path = os.path.join(UPLOAD_FOLDER, file.filename)
-        file.save(img_path)
+        captions = []
+        image_urls = []
+        gpt_audio_urls = []
+        for file in files:
+            if file:
+                img_path = os.path.join(UPLOAD_FOLDER, file.filename)
+                file.save(img_path)
 
-        feature = extract_features(img_path)
-        caption = generate_caption(feature)
+                # 이미지에서 특징 추출 및 캡션 생성
+                feature = extract_features(img_path)
+                caption = generate_caption(feature)
+                captions.append(caption)
+                image_urls.append(img_path)
 
-        return render_template('index.html', caption=caption, image_path=url_for('static', filename='uploads/' + file.filename))
+        # GPT로 캡셔닝된 텍스트 처리
+        combined_captions = '\n'.join([f"{i+1}번: {caption}" for i, caption in enumerate(captions)])
+        thread = openai_client.beta.threads.create()
+        message = openai_client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=combined_captions
+        )
+        run = openai_client.beta.threads.runs.create_and_poll(
+            thread_id=thread.id,
+            assistant_id='',
+            instructions="You are given a set of words that summarize all the pages of a storybook and must summarize the content of each page. When given a list of korean words or a theme, you make short story which is delightful and age-appropriate narrative in korean. each story should be less than 20 words. each story should be connected well previous and next summary. You aim to make short story while also subtly teaching lessons or morals. Avoid any content that could be frightening or inappropriate for young children. total summary should be less than 100 words. you should make summary in korean."
+        )
+        if run.status == 'completed':
+            messages = openai_client.beta.threads.messages.list(thread_id=thread.id)
+            gpt_result_text = messages.data[0].content[0].text.value
+            gpt_results = gpt_result_text.split('\n')
 
-@app.route('/caption', methods=['POST'])
-def caption_image():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image uploaded'}), 400
-    img = request.files['image']
-    img_path = os.path.join(UPLOAD_FOLDER, img.filename)
-    img.save(img_path)
-
-    feature = extract_features(img_path)
-    caption = generate_caption(feature)
-    return jsonify({'caption': caption})
+            for i, gpt_result in enumerate(gpt_results):
+                clean_gpt_result = gpt_result.split(':', 1)[1].strip()  # 'n번:' 제거
+                audio_file_path = os.path.join(AUDIO_FOLDER, f'gpt_audio_{i+1}.mp3')
+                text_to_speech(clean_gpt_result, audio_file_path)
+                gpt_audio_urls.append(audio_file_path)
+            
+            # JSON 응답에 텍스트 정보와 오디오 파일 경로를 포함
+            response_data = {
+                "image_urls": image_urls,
+                "captions": captions,
+                "gpt_results": gpt_results,
+                "gpt_audio_urls": gpt_audio_urls
+            }
+            return jsonify(response_data)
+        else:
+            return jsonify({'error': run.status}), 500
+    except Exception as e:
+        print(f"Error processing images: {str(e)}")
+        return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
